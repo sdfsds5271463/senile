db74b7b4b67d1e4eda801b7c3684076a479c5ab5
allen <sdfsds5271463@gmail.com>
2026-04-05 02:03:54 +0800

    fix ssr3

--- resources/js/Pages/testView.vue
+++ resources/js/Pages/testView.vue
@@ -13,7 +13,7 @@
 </script>
 
 <template> 
-    {{ t01 }} {{ testTs(uri ?? '') }} ---test nnn4---
+    {{ t01 }} {{ testTs(uri ?? '') }} ---test nnn5---
     <div class="card">
         <DataTable :value="[]" paginator :rows="10" tableStyle="min-width: 50rem">
             <Column field="symbol" header="測試UI表格1"></Column>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    ssr: {
+        // 將所有 npm dependencies 打包進 SSR bundle
+        // 讓 bootstrap/ssr/ssr.js 完全自包含，不需要 node_modules 存在
+        noExternal: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
