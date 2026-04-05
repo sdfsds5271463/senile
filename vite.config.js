f20c79d936bd2c3a7dc40938d4d77d25219f567c
allen <sdfsds5271463@gmail.com>
2026-04-06 01:57:54 +0800

    fix ui load css

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
