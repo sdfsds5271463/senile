ee0e4d1f2622a697e2aab58bde08f44f53ba030f
allen <sdfsds5271463@gmail.com>
2026-04-09 15:32:59 +0800

    tempo fix 6

--- infra/terraform/02-platform/tempo.tf
+++ infra/terraform/02-platform/tempo.tf
@@ -10,7 +10,7 @@ resource "helm_release" "tempo" {
   name       = "tempo"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "tempo"
-  version    = "1.10.3"
+  version    = "1.14.0"   # Tempo 2.7.x，支援 compare() TraceQL 函式
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   wait       = true
   timeout    = 180
