15015b59fbb9bfab01a96b6e23f1f03ab6740a6c
allen_in_vm <allen_in_vm@example.com>
2026-04-11 23:43:56 +0800

    feat: laravel horizon instelled

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\PrometheusServiceProvider::class,
     App\Providers\TracingServiceProvider::class,
 ];
